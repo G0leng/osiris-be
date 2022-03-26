75894b23ebb361a0b73f5771c840952454dbf700
Gaile <gailesarmiento@gmail.com>
2022-03-26 11:13:23 +0800

    Add missing routes to institution

--- src/api/institution/routes/institution.js
+++ src/api/institution/routes/institution.js
@@ -20,6 +20,32 @@ module.exports = {
             method:'GET',
             path:'/institution/findSchoolCode/:institutionCode',
             handler:'institution.findSchoolCode',
-        }
+        },
+        {
+            method: 'GET',
+            path: '/institutions',
+            handler: 'institution.find',
+          },
+          {
+            method: 'GET',
+            path: '/institutions/:id',
+            handler: 'institution.findOne',
+          },
+          {
+            method: 'POST',
+            path: '/institutions',
+            handler: 'institution.create',
+          },
+          {
+            method: 'PUT',
+            path: '/institutions/:id',
+            handler: 'institution.update',
+          },
+          {
+            method: 'DELETE',
+            path: '/institutions/:id',
+            handler: 'institution.delete',
+          },
+          
     ]
 }
