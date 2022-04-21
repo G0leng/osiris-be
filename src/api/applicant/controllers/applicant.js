025a591638c313af90e59202b87dca2251ad42ed
Gaile <gailesarmiento@gmail.com>
2022-04-22 02:46:38 +0800

    +Add school year, grades to students

--- src/api/applicant/controllers/applicant.js
+++ src/api/applicant/controllers/applicant.js
@@ -28,9 +28,58 @@ module.exports = createCoreController('api::applicant.applicant', ({strapi})=>({
 
     findOne: async (ctx) => {
         const id = ctx.params.id;
+        //TODO: add info if already a student
         const aData = await strapi.service('api::applicant.applicant').findOne(id, {populate:{institution:true, payment_plan:true}});
+        if(aData.admissionStatus){
+            console.log("Approved as student");
+            const sData = await strapi.service('api::student.student').find( {
+                filters:{applicantId:id},
+                populate:{institution:true, branch:true, payment_plan:true, grades:true}
+            });
+            
+            return sData.results[0];
+        }
         console.log("ADTA: ", aData);
         return aData;
+    },
+
+    approve: async (ctx) => {
+        const id = ctx.params.id;
+        console.log("Applicant ID", id);
+        const applicantData = await strapi.service('api::applicant.applicant').update(id, ctx.request.body);
+        const aData = await strapi.service('api::applicant.applicant').findOne(id, {populate:{institution:true, branch:true, payment_plan:true, guardian:true }});
+        console.log("Applicant Data", aData);
+        if(ctx.request.body.data.admissionStatus){
+            const sData = aData;
+            console.log("Status TRUE");
+            //auto generate student ID
+            const randId = (new Date().getTime()); 
+            const studentId = "2022-"+randId+"-"+id;
+            console.log("Student ID: ", studentId);
+            if (typeof sData.createdAt !== 'undefined') {
+                delete sData.createdAt;
+            }
+            if (typeof sData.updatedAt !== 'undefined') {
+                delete sData.updatedAt;
+            }
+            //auto generate email
+            const email = studentId+"@headstartsolutionsph.com"
+            sData.studentId = studentId;
+            sData.email = email;
+            sData.applicantId = parseInt(id);
+            sData.guardian =  parseInt(aData.guardian.id);
+            sData.institution = parseInt(aData.institution.id);
+            sData.branch = parseInt(aData.branch.id);
+            sData.payment_plan = parseInt(aData.payment_plan.id)
+            sData.publishedAt = new Date()
+            console.log("Student Data: ", sData);
+            //return aData;
+            
+            //TODO: remove create and published date before inserting
+            const studentData = await strapi.service('api::student.student').create({data:sData});
+            return studentData;   
+        }
+        return applicantData;
     }
     
 }));
--- src/api/applicant/routes/applicant.js
+++ src/api/applicant/routes/applicant.js
@@ -2,8 +2,44 @@
 
 /**
  * applicant router.
- */
+ 
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
 module.exports = createCoreRouter('api::applicant.applicant');
+*/
+
+module.exports = {
+    routes:[
+        {
+            method: 'GET',
+            path: '/applicants',
+            handler: 'applicant.find',
+          },
+          {
+            method: 'GET',
+            path: '/applicants/:id',
+            handler: 'applicant.findOne',
+          },
+          {
+            method: 'POST',
+            path: '/applicants',
+            handler: 'applicant.create',
+          },
+          {
+            method: 'PUT',
+            path: '/applicants/:id',
+            handler: 'applicant.update',
+          },
+          {
+            method: 'DELETE',
+            path: '/applicants/:id',
+            handler: 'applicant.delete',
+          },
+          {
+            method: 'PUT',
+            path: '/applicants/approve/:id',
+            handler: 'applicant.approve',
+          },
+    ]
+}
