062bb12d449a5669dedf63daa01637f80bbe98a7
Gaile <gailesarmiento@gmail.com>
2022-04-22 07:12:02 +0800

    *Edit payment relationship with student

--- src/api/payment/controllers/payment.js
+++ src/api/payment/controllers/payment.js
@@ -6,4 +6,19 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::payment.payment');
+module.exports = createCoreController('api::payment.payment', ({strapi})=>({
+    /*
+    create: async (ctx) => {
+        const dataRequest = JSON.parse(ctx.request.body.data)
+        console.log("Body: ", dataRequest);
+        console.log("Student ID: ", dataRequest.student);
+    
+        const paymentData = await strapi.service('api::payment.payment').create(dataRequest);
+        console.log("Payment Data Json", paymentData);
+        return paymentData;
+        
+    },
+    */
+    
+
+}));
--- src/api/payment/routes/payment.js
+++ src/api/payment/routes/payment.js
@@ -2,8 +2,39 @@
 
 /**
  * payment router.
- */
+ 
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
 module.exports = createCoreRouter('api::payment.payment');
+*/
+
+module.exports = {
+    routes:[
+        {
+            method: 'GET',
+            path: '/payments',
+            handler: 'payment.find',
+          },
+          {
+            method: 'GET',
+            path: '/payments/:id',
+            handler: 'payment.findOne',
+          },
+          {
+            method: 'POST',
+            path: '/payments',
+            handler: 'payment.create',
+          },
+          {
+            method: 'PUT',
+            path: '/payments/:id',
+            handler: 'payment.update',
+          },
+          {
+            method: 'DELETE',
+            path: '/payments/:id',
+            handler: 'payment.delete',
+          },
+    ]
+}
