1049605e146a7c78276f6f414be8687d88e5e4ee
Gaile <gailesarmiento@gmail.com>
2022-04-22 08:05:20 +0800

    *Change grades schema

--- src/api/grade/controllers/grade.js
+++ src/api/grade/controllers/grade.js
@@ -6,4 +6,12 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::grade.grade');
+module.exports = createCoreController('api::grade.grade', ({strapi})=>({
+    findOne: async (ctx) => {
+        const id = ctx.params.id;
+        //TODO: add info if already a student
+        const aData = await strapi.service('api::grade.grade').findOne(id, {populate:{subject:true, school_year:true}});
+        console.log("ADTA: ", aData);
+        return aData;
+    },
+}));
--- src/api/student/controllers/student.js
+++ src/api/student/controllers/student.js
@@ -6,4 +6,13 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::student.student');
+module.exports = createCoreController('api::student.student', ({strapi})=>({
+    findOne: async (ctx) => {
+        const id = ctx.params.id;
+        //TODO: add info if already a student
+        const aData = await strapi.service('api::student.student').findOne(id, {populate:{institution:true, branch:true, payment_plan:true, grades:true, payments:true}});
+
+        console.log("ADTA: ", aData);
+        return aData;
+    },
+}));
